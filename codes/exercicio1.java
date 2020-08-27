3cb2a7cc0d0acd27fcc2c54cbe14b158d14e0c10
Anne Nogueira <62261535+annie-bot@users.noreply.github.com>
2020-08-27 11:21:06 -0300

    Create exercicio1.java

---
+++ codes/exercicio1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+
+    float value1 = entrada.nextFloat();
+    float value2 = entrada.nextFloat();
+
+    if(val1 > val2){
+      System.out.println("Valor 1 = "+value1);
+    }
+    else if (value1 == value2){
+      System.out.println("Os valores são iguais");
+    }
+    else 
+    System.out.println("Valor 2 = "+ val2);
+  
+  }
+}
+
+//Escreva um programa que recebe do usuário dois valores do tipo ponto flutuante e imprime na tela qual dos dois é maior.
+
