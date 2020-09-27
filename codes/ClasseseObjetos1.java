f99fa97d49256c9f6d891894f09b10d3ccb6c7ee
Anne Nogueira <62261535+annie-bot@users.noreply.github.com>
2020-09-27 19:36:31 -0300

    Update ClasseseObjetos1.java

--- codes/ClasseseObjetos1.java
+++ codes/ClasseseObjetos1.java
@@ -65,3 +65,22 @@ public class Carro {
 		
 	}
 }
+
+public class lojaDeCarro {
+
+	public static void main(String[] args) {		
+//		Fiat Pálio básico, de preço básico R$ 35000
+//		Fiat Pálio básico com cor prateada, de preço básico R$ 35000
+//		Honda Civic completo, de preço básico R$110000
+//		Toyota Corolla com vidro, direção e ar, de preço básico R$110000
+//		Volkswagen Gol completo, de preço básico R$ 55000 (use o construtor vazio)
+
+		Carro c1 = new Carro("Pálio", "Fiat", 35000);
+		Carro c2 = new Carro("Pálio", "Fiat", "PRATA", 2, false, false, false, false, 35000);
+		Carro c3 = new Carro("Civic", "Honda", "AMARELA", 4, true, true, true, true, 110000);
+		Carro c4 = new Carro("Corolla", "Toyota", "AMARELA", 4, true, true, false, true, 110000);
+		Carro c5 = new Carro("Gol", "Volkswagen", "VERDE", 4, true, true, true, true, 55000);
+
+		System.out.println(c4);
+
+	}
